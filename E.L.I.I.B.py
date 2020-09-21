a3c4313f9b556f226f6a44696798f0718777f234
darkClaw921 <54072056+darkClaw921@users.noreply.github.com>
2020-09-22 01:15:00 +0300

    add messages.getHistory
    перестал работать метод messages.getHistoryAttachments сделал через messages.getHistory

--- E.L.I.I.B.py
+++ E.L.I.I.B.py
@@ -20,7 +20,7 @@ from threading import Thread
 # Help files import
 import setings
 
-# VK settings
+# VK setings
 vk_session = vk_api.VkApi(token = setings.vkDdkgtaApi)
 longpoll = VkLongPoll(vk_session)
 vk = vk_session.get_api()
@@ -36,7 +36,12 @@ rowSecondName = 2
 rowQuestions1 = 2
 rowQuestions2 = 2
 rowQuestions3 = 2
+rowQuestions4 = 2
+rowQuestions5 = 2
+rowQuestions6 = 2
 rowWork = 2
+photoGet = ['a']
+void = 0
 
 # Create sheet
 print('Создание таблицы ...')
@@ -51,7 +56,9 @@ sheet.update_cell(1, 3, "Фамилия")
 sheet.update_cell(1, 4, "Вопрос 1")
 sheet.update_cell(1, 5, "Вопрос 2")
 sheet.update_cell(1, 6, "Вопрос 3")
-sheet.update_cell(1, 7, "Задание 1")
+sheet.update_cell(1, 7, "Вопрос 4")
+sheet.update_cell(1, 8, "Вопрос 5")
+sheet.update_cell(1, 9, "Вопрос 6")
 print('Таблица создана')
 
 # Functional
@@ -87,12 +94,10 @@ def keyboardCreater(ButtonText1, ButtonText2, ButtonText3, ButtonText4):
     return keyboard
 
 def getQuestion(randomId, userID):
-    global rowId, rowQuestions1, rowName, rowSecondName, rowWork
+    global rowId, rowQuestions1, rowName, rowSecondName, rowWork, void, photoGet
 
     userInfo = vk.users.get(user_ids = userID) # Получили ответ в виде массива из одного списка
     userInfo = userInfo[0] 
-    
-    keyboard = keyboardCreater("13", "2", "123", "9")
     
     sheet.update_cell(rowId, 1, f"""vk.com/id{userID}""")
     sheet.update_cell(rowName, 2, userInfo["first_name"])
@@ -108,48 +113,65 @@ def getQuestion(randomId, userID):
                     random_id = randomId,
                     #attachment='photo-194390511_457239038', # ссылк фото из альбома сообщества
                     message = """
-Сколько членов квартиры 84 ?
+Привет, первокурсник. Этот квест создан для того, чтобы ты улыбнулся, немного поднапряг извилины и сделал на своем смартфоне пару прикольных фото. Если будешь не лениться - закончишь КГТА. А если будешь не лениться в квесте - получишь приз! Удачи! Держи первое задание:
                             """,
-                    keyboard = keyboard.get_keyboard()
+                    # keyboard = keyboard.get_keyboard()
 		            )
+
+    vk.messages.send( #Отправляем сообщение
+                    user_id = userID,
+                    random_id = randomId,
+                    #attachment='photo-194390511_457239038', # ссылк фото из альбома сообщества
+                    message = """
+Вчера на собрании каждому студенту-первокурснику давали навигатор по вузу. Сделай селфи с ним. Фото присылай сюда!
+                            """,
+                    # keyboard = keyboard.get_keyboard()
+		            )
+    getMessege("", userID)
+    time.sleep(0)
+    messageID = vk.messages.getHistory(user_id = userID, count = 1)
+    print(messageID['items'])
+    messageID = messageID['items']
+    messageID = messageID[0]
+    messageID = messageID['attachments']
+    messageID = messageID[0]
+    messageID = messageID['photo']
+    messageID = messageID['sizes']
+    messageID = messageID.pop()
+    messageID = messageID['url']
+    print('history ', messageID )
+
+    print("получение фото от 1 вопроса")
+   
+
+    sheet.update_cell(rowQuestions1, 4, f"""=IMAGE("{str(messageID)}")""")
+    rowQuestions1 += 1
     
-    otvetQvestions = getMessege("9", userID) # Ответ который получили от пользователя 
-
-    if otvetQvestions :
-        countPoint += 1
-        print("getQuestion: ", countPoint)
-
-        sheet.update_cell(rowQuestions1, 4, "1")
-        rowQuestions1 += 1
-    else: 
-        print("getQuestion: ", countPoint)
-
-        sheet.update_cell(rowQuestions1, 4, "0")
-        rowQuestions1 += 1
-
+    void = 0
+    photoGet = ['a']
     countPoint += getQuestion2(randomId, userID)
 
-    sendMessege(userID, randomId, "Не,ра,страий,ся")
+    sendMessege(userID, randomId, "Поздравляю! Все испытания позади! Если ты был честен, умен и креативен со мной, то жди сообщение о призе. Твой бот – Валера. Надеюсь еще спишемся")
     print("Finish getQuestion: ", countPoint)
 
 def getQuestion2(randomId, userID):
     global rowQuestions2
 
-    keyboard = keyboardCreater("ППО", "Квартира", "84", "CТО")
+    keyboard = keyboardCreater("87", "90", "72", "88")
 
     countPoint = 0
 
     vk.messages.send( #Отправляем сообщение
                     user_id = userID,
                     random_id = randomId,
-                    # attachment = 'photo-194390511_457239038',
+                    attachment = 'photo-194390511_457239113',
                     message="""
-Что такое Квартира 84 ?
+Не хотелось тебя грузить чем-то сложным, поэтому вот тебе детская задачка. Какой номер у паковочного места под машиной
                             """,
                     keyboard = keyboard.get_keyboard()
 		            )
 
-    otvetQvestions = getMessege("ППО", userID)
+    otvetQvestions = getMessege("87", userID)
 
     if otvetQvestions:
         countPoint += 1
@@ -168,67 +190,143 @@ def getQuestion2(randomId, userID):
     return countPoint
 
 def getQuestion3(randomId, userID):
-    global rowQuestions3
-
-    keyboard = keyboardCreater("Один дома", "27 Стренджовят", "Война бесконечности", "Миша")
+    global rowQuestions3, void, photoGet
 
     countPoint = 0
 
     vk.messages.send( #Отправляем сообщение
                     user_id = userID,
                     random_id = randomId,
-                    attachment = 'photo-194390511_457239097',
+                    # attachment = 'photo-194390511_457239113',
                     message="""
-Из какого это фильма?
+Знаешь своего студенческого тьютора? Тот старшекурсник, который прикреплен к твоей группе. У каждой группы он свой. Дружи с ним. Давай начнем с ним общаться. Найди своего тьютора в вк. Напиши какое-нибудь сообщение и дождись ответа. Скрин вашей переписки закинь сюда, и задание будет выполнено.
                             """,
-                    keyboard = keyboard.get_keyboard()
+                    # keyboard = keyboard.get_keyboard()
 		            )
 
-    otvetQvestions = getMessege("Война бесконечности", userID)
+    getMessege("", userID)
+    time.sleep(0)
 
-    if otvetQvestions :
-        countPoint += 1
-        print("getQuestion3: ", countPoint)
+    messageID = vk.messages.getHistory(user_id = userID, count = 1)
+    print(messageID['items'])
+    messageID = messageID['items']
+    messageID = messageID[0]
+    messageID = messageID['attachments']
+    messageID = messageID[0]
+    messageID = messageID['photo']
+    messageID = messageID['sizes']
+    messageID = messageID.pop()
+    messageID = messageID['url']
+    print('history ', messageID )
 
-        sheet.update_cell(rowQuestions3, 6, "1")
-        rowQuestions3 += 1 
-    else :
-        print("getQuestion3: ", countPoint)
-        sheet.update_cell(rowQuestions3, 6, "0")
-        rowQuestions3 += 1 
+    print("получение фото от 2 вопроса")
+   
+
+    sheet.update_cell(rowQuestions3, 6, f"""=IMAGE("{str(messageID)}")""")
+    rowQuestions3 += 1
+    void = 0
+    photoGet = 0
     
     getQuestion4(randomId, userID)
 
     return countPoint
 
 def getQuestion4(randomId, userID):
-    global rowQuestions2, rowWork
+    global rowQuestions4
+    
+    countPoint = 0
+
+    keyboard = keyboardCreater("30p", "27p", "20p", "У нас есть буфет?")
 
     vk.messages.send( #Отправляем сообщение
                     user_id = userID,
                     random_id = randomId,
                     #attachment = 'photo-194390511_457239038',
                     message="""
-Отправь любое фото
+Молодец, не прошло и дня. Пора подкрепиться в студенческом буфете. Кстати, именно там ты справишься со следующим заданием. Узнай, сколько стоит лимонад в студенческом буфете:
+                            """,
+                    keyboard = keyboard.get_keyboard()
+		            )
+
+    otvetQvestions = getMessege("30p", userID)
+
+    if otvetQvestions :
+        countPoint += 1
+        print("getQuestion4: ", countPoint)
+
+        sheet.update_cell(rowQuestions4, 7, "1")
+        rowQuestions4 += 1 
+    else :
+        print("getQuestion4: ", countPoint)
+        sheet.update_cell(rowQuestions4, 7, "0")
+        rowQuestions4 += 1 
+
+    getQuestion5(randomId, userID)
+
+def getQuestion5(randomId, userID):
+    global rowQuestions5
+
+    vk.messages.send( #Отправляем сообщение
+                    user_id = userID,
+                    random_id = randomId,
+                    # attachment = 'photo-194390511_457239038',
+                    message="""
+Первокурсник, ты не устал? Еще немного. Это задание – вообще элементарное. Напиши чего ты ждешь от своей студенческой жизни? (Одним собщением)
+                            """,
+                    # keyboard = keyboard.get_keyboard()
+		            )
+
+    def getMessege1 (stringOtvet, user_id): # Получаем сообщение от конкретного пользователя
+        for event in longpoll.listen(): # цикл для каждго ивента сервера
+            if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.user_id == user_id: # ждать ответа от данного юзера 
+                vk.messages.getConversations(offset = 0, count = 1)  
+        
+                if event.text == stringOtvet: # если событие текст и он равен сообщению которое отправил пользователь
+                    return event.text
+
+                return event.text
+
+    otvetQvestions5 = getMessege1("", userID)
+
+    sheet.update_cell(rowQuestions5,8,otvetQvestions5)
+    rowQuestions5 += 1
+
+    getQuestion6(randomId, userID)
+
+def getQuestion6(randomId, userID):
+    global rowQuestions6
+
+    vk.messages.send( #Отправляем сообщение
+                    user_id = userID,
+                    random_id = randomId,
+                    #attachment = 'photo-194390511_457239038',
+                    message="""
+Есть инстаграмм? Если нет - самое время попробовать завести. Сними в нем сторис, отметь @profkom_kgta, скрин пришли сюда!
                             """
 		            )
+
     getMessege("", userID)
+    time.sleep(0)
 
-    photoGet = vk.messages.getHistoryAttachments(peer_id = userID ,media_type='photo', start_from = 'photo', cout=1 ) # Получить от пользователя последние фото
-    
-    void = photoGet['items'] # распарсили ответ от photoGet
-    void =  void[0]
-    void =  void['attachment']
-    void =  void['photo']
-    void =  void['sizes']
-    void =  void.pop() # последний элемент списка
-    void =  void['url']
+    messageID = vk.messages.getHistory(user_id = userID, count = 1)
+    print(messageID['items'])
+    messageID = messageID['items']
+    messageID = messageID[0]
+    messageID = messageID['attachments']
+    messageID = messageID[0]
+    messageID = messageID['photo']
+    messageID = messageID['sizes']
+    messageID = messageID.pop()
+    messageID = messageID['url']
 
-    print(void)
+    print('history ', messageID )
 
-    sheet.update_cell(rowWork, 7, f"""=IMAGE("{str(void)}")""")
-    rowWork += 1
-                  
+    print("получение фото от 1 вопроса")
+   
+
+    sheet.update_cell(rowQuestions6, 9, f"""=IMAGE("{str(messageID)}")""")
+    rowQuestions6 += 1
+
 def newUser():
     print("Проверка подключения")
 
@@ -246,7 +344,7 @@ def newUser():
 for event in longpoll.listen():
     
     print(event.type)
-
+    
     if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
 
         whoUser = newUser()
